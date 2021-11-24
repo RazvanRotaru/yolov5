247914e0f48a106907cb15effd59cda1a5550725
Razvan Rotaru <razvanrtr@outlook.com>
2021-11-24 14:54:57 +0200

    again

--- utils/plots.py
+++ utils/plots.py
@@ -465,6 +465,7 @@ def save_one_box(xyxy, im, file='image.jpg', gain=1.02, pad=10, square=False, BG
     crop = im[int(xyxy[0, 1]):int(xyxy[0, 3]), int(xyxy[0, 0]):int(xyxy[0, 2]), ::(1 if BGR else -1)]
     if save:
         file.parent.mkdir(parents=True, exist_ok=True)  # make directory
+        print(increment_path(file))
         cv2.imwrite(str(increment_path(file).with_suffix('.jpg')), crop)
         torch.save(str(increment_path('bboxes.txt').with_suffix('.txt')), xyxy)
     return crop
